3856bb63ecb245329f7c02be0c4d34e8d8347862
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-30 13:06:57 +0500

    Implement first test using jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,1 +1,20 @@
-it('Hello Jest', () => {});
+const googleSearch = require('./index');
+
+const dbMock = ['dog.com', 'cheesepuf.com', 'disney.com', 'dogspicture.com'];
+
+describe('Group Togather : ', () => {
+  it('Google Search ...', () => {
+    expect(googleSearch('cheesepuf', dbMock)).toEqual(['cheesepuf.com']);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogspicture.com']);
+    expect(googleSearch('dg', dbMock)).toEqual([]);
+  });
+
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('Does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toBe(3);
+  });
+});
