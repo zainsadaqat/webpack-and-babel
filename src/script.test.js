30f1bf0580bae7e3c66eb9b2f66963165fdf2f56
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-30 14:27:21 +0500

    Implement tests for asynchronous code

--- src/script.test.js
+++ src/script.test.js
@@ -2,7 +2,16 @@ const fetch = require('node-fetch');
 const swapi = require('./script');
 
 it('call swapi api to get people', () => {
-  swapi.getPeoplePromise(fetch).then((data) => {
-    expect(data.count).toEqual(87);
+  expect.assertions(1); // for asynchronous code, 1 means no. of expect to run
+  return swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+  });
+});
+
+it('call swapi api to get people from Promise', () => {
+  expect.assertions(2);
+  return swapi.getPeoplePromise(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
   });
 });
