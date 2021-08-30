5b7bb50850c2da96d5bbc5105045ada9ea547dc0
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-30 13:07:38 +0500

    Add --watch to test scripts

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ const googleDatabase = [
   'spicysoup.com',
 ];
 
-const googleSearch = (searchInput) => {
-  const matches = googleDatabase.filter((website) => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch('cats'));
+// console.log(googleSearch('cat', googleDatabase));
+
+module.exports = googleSearch;
